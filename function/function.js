fbd91b48e0320df2bf3cd5eced66ef06561bd95d
Umang Tibrewal <87933066+umang-tibrewal@users.noreply.github.com>
2023-10-06 18:05:39 +0000

    beginning

--- function/function.js
+++ function/function.js
@@ -1,12 +1,8 @@
 function add(number1,number2){
-
     return number1+number2
-
-
 }
 let result=add(54,10)
 console.log(result);
-
 
 let  funobj={
 
@@ -18,12 +14,10 @@ function printName({name:n,age:a}){
 
     console.log(`my name is ${n} i am ${a} years old`);
 }
-
-
 printName(funobj)
-
-
 callexplain(53)
+
+
 function callexplain(number){
 
     console.log(number)
@@ -36,3 +30,69 @@ function callexplain(number){
 // console.log(num);
 
 // }
+
+
+let funex=function(){
+
+    return(4+6);
+}
+console.log(funex());
+// -----------------------------------------------------------------------------------------------------------------------------------------
+
+// concept of this ->it represent the current state
+
+let exampleobj={
+
+    name:"umang",
+
+    welcome:function (){
+
+        console.log(`welcome ${this.name}`);
+        console.log(this);
+    }
+  
+}
+
+console.log(exampleobj.welcome());
+
+// this donot work inside function 
+
+// let ex=function (){
+
+//     let name ="umang";
+
+//     console.log(`my name is ${this.name}`);
+//     console.log(this);
+// }
+// ex()
+
+// function thisConcept(){
+//     console.log(this);
+// }
+
+// thisConcept()
+
+// this will return long value 
+// ---------------------------------------------------------------------------------------------------------------------------
+let arowThis=()=>{
+
+    console.log(this);
+}
+arowThis()
+// return empty parenthesis
+
+// let addTwonumbers=()=>{
+
+//     return (4+5)
+// }
+//  explicit arrow function
+
+
+// let addTwonumbers=()=>(5+5)
+//implicit return function
+
+// but when deal with object make sure to wrap in {}
+let addTwonumbers=()=>({username:"umang"})
+
+
+console.log(addTwonumbers());
---
+++ function/iife.js
@@ -0,0 +1,14 @@
+// intermidiately invoke function
+// to imediate execution of function 
+// to avoide global polution 
+
+
+(function chai (){
+
+  console.log("i am learning code with hitesh sir ");
+})();
+
+(function chai (name){
+
+    console.log(`i am learning code with ${name} sir `);
+  })("hitesh");
