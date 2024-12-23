313202098c19137326d46b14b5af47cb28d02d8c
Umang Tibrewal <87933066+umang-tibrewal@users.noreply.github.com>
2024-12-24 01:37:42 +0530

    Update function.js

--- function/function.js
+++ function/function.js
@@ -96,3 +96,52 @@ let addTwonumbers=()=>({username:"umang"})
 
 
 console.log(addTwonumbers());
+
+
+/*-------------------------------------------------------------------------------------------------------------------------------------------------------
+An Important Observation 
+
+When dealing with function inside an object 
+let obj={
+  
+  name:"umang",
+  classes:"24",
+  location:"kolkata",
+  greting: function(){
+    
+    console.log(`Welcome to javascript learning ${this.name}`)
+    return 0;
+  }
+}
+
+console.log(obj.greting())
+This will give expected Result 
+Welcome to javascript learning umang
+0
+But
+let obj={
+  
+  name:"umang",
+  classes:"24",
+  location:"kolkata",
+  greting: ()=>{
+    
+    console.log(`Welcome to javascript learning ${this.name}`)
+    return 0;
+  }
+}
+
+console.log(obj.greting())
+
+Welcome to javascript learning undefined
+0
+
+Explanation 
+Regular functions create their own this context, which in an object method refers to the object itself
+Arrow functions inherit this from their surrounding scope (lexical scope), they don't create their own this context
+
+
+
+
+
+*/
